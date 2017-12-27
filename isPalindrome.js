85e2cb4c2f0c4dc7abd8a362bfc84f27b31c62af
Amit Ashutosh <amit.ashutosh@hotmail.com>
2017-12-26 16:38:35 -0800

    isPalindrome in JS

---
+++ isPalindrome.js
@@ -0,0 +1,23 @@
+/**
+    Problem: Test if a given string is a palindrome.
+    Example: "abccba" is a palindrome. "racecar" is a palindrome. "abcde" is not a palindrome.
+
+    Variation 1: Given a string with white spaces and alpha numerics characters, test that a string is a palindrome. Note that any numeric and white spaces characters does not count into consideration and can be ignored. For example, "a bcba".
+
+    Variation 2: Given an integer, test that 121 is a palindrome, 123 is NOT, 1023201 is a palindrome.
+**/
+
+function isPalindrome(str) {
+    var str = str;
+
+}
+
+
+// Test cases
+isPalindrome("");
+isPalindrome("a");
+isPalindrome("aba");
+isPalindrome("aaaa");
+isPalindrome("abcde");
+isPalindrome("ab");
+isPalindrome("123");
