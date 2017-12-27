0e01bd93f2499f53d8588ac3614c51fd8cd8aac7
Amit Ashutosh <amit.ashutosh@hotmail.com>
2017-12-26 17:07:56 -0800

    Reverse string - 1st commit

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+/**
+    Problem: Reverse a string. For example, Reverse("helloworld") returns "dlrowolleh".
+
+    Variation 1: Reverse words in a string. Reverse("hello world") returns "world hello".
+    Variation 2: Reverse digits in integer. Reverse(12345) returns 54321.
+    Variation 3: Reverse binary digits of 32 bit integer. Reverse(0x00000005), returns 0x5000000.
+**/
+
+function reverse(str) {
+    
+}
